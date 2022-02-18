c250c603c4dd8c6f0287b6ef5ca5bf40e9f3abe9
mgkeze <mgkeze@gmail.com>
2022-02-18 23:06:32 +0600

    first project

---
+++ src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Я создал это приложение, чтобы не подводить свою команду и дать толчок к фреймворку vue3</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
